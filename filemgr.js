863776e82dbf1d6bf7b2cefb2617e64c5c3759c4
Kuganeshan <kuganeshan.nair@gmail.com>
2018-07-18 17:36:19 +0800

    Replaces files

--- filemgr.js
+++ filemgr.js
@@ -1,9 +1,9 @@
 const {MongoClient}= require('mongodb');
 const fs = MongoClient;
 
-//const database = 'mongodb://localhost:27017';
+const database = 'mongodb://localhost:27017';
 //const database = 'mongodb://KN:iamkugan27@ds245150.mlab.com:45150/weather_app';
-const database = 'mongodb://Wopla:123456abc@ds139951.mlab.com:39951/woplaweather';
+//const database = 'mongodb://Wopla:123456abc@ds139951.mlab.com:39951/woplaweather';
 
 const saveData = (newdata) => {
   return new Promise((resolve, reject) => {
